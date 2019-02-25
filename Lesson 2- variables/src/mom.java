f95cbccc3cd6670797c3343cfd908fd2c4be1ca8
rsullivan <rsullivan@HS-D210-15.MOHS.local>
2019-02-25 13:23:15 -0500

    your dada

--- Lesson 2- variables/src/mom.java
+++ Lesson 2- variables/src/mom.java
@@ -3,15 +3,25 @@ public class mom {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int age = 18;
+		String ager = "Age =";
+		String height = "6 Foot 4 Inches";
+		String heightR = "height =";
+		String weight = "182lbs";
+		String weightP = "Weight =";
+		int GPA = -7;
+		String GPAR = "GPA =";
+		String haircolor = "Dirty Blonde";
+		String haircolorr = "Hair Color =";
+		String daddy = "Grossi";
+		String daddyr = "Daddy =";
+ System.out.println(ager + " "+age);
+ System.out.println(heightR + " " + height);
+ System.out.println(weightP + " "+ weight);
+ System.out.println(GPAR + " " + GPA);
+ System.out.println(haircolorr + " "+ haircolor);
+System.out.println(daddyr + " " + daddy);
+	
+	}
 
-	}
-int age = 18;
-String height = "6 Foot 4 Inches";
-String weight = "182lbs";
-int GPA = -7;
-String haircolor= "Dirty Blonde";
-String daddy = "Grossi";
-//sytem.out.println( age + " " + height + " " + weight + " " + GPA + " " +  haircolor + " " + daddy ) ;
- //System.out.println("dad");
- system.out.println("Grossi"); 
-	}
+}
