37aeb0275fdbda3d80ec4c1c0f44b3163a852c55
rsullivan <rsullivan@HS-D210-15.MOHS.local>
2019-02-26 12:12:55 -0500

    your mom! Ha GOTIIEEE

--- Lesson 3- string manipulation/src/stringManipulation.java
+++ Lesson 3- string manipulation/src/stringManipulation.java
@@ -3,12 +3,12 @@ public class stringManipulation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String name = "Ryan Sullivan";
-System.out.println(name.length()-1);
-System.out.println(name.substring(5));
-String lname = name.substring(4);
-String fname = name.substring(0,5);
-System.out.println(fname + lname);
+		/*
+		 * String name = "Ryan Sullivan"; System.out.println(name.length()-1);
+		 * System.out.println(name.substring(5)); String lname =
+		 * name.substring(4); String fname = name.substring(0,5);
+		 * System.out.println(fname + lname);
+		 */
 	}
 
 }
