229d8a2a0cdb97eb5857828f4b17a0a9bf41f92b
rsullivan <rsullivan@HS-D210-15.MOHS.local>
2019-02-26 12:14:09 -0500

    your mom GOTIIEEE!

---
+++ Lesson 2- variables/src/notherone.java
@@ -0,0 +1,21 @@
+
+public class notherone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String me = "Ryan Sullivan - ";
+String details = "MOHS Student"; 
+String grounder = "Ground Balls= ";
+int ground = 97633;
+String name = "gbs";
+String teacher = " Grossi";
+String bestest = "Best Teacher-";
+String wood = "Days Spent in Woodshop= ";
+int days = 300;
+System.out.println(me + details);
+System.out.println(grounder + ground+name);
+System.out.println(bestest + teacher);
+System.out.println(wood + days);
+}
+
+}
