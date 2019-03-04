bd37ba7ea24c075ffb08256004ebda057e0ddc2d
rsullivan <rsullivan@HS-D210-15.MOHS.local>
2019-03-04 12:38:14 -0500

    whos your daddy

--- Lesson 3- string manipulation/src/stringManipulation.java
+++ Lesson 3- string manipulation/src/stringManipulation.java
@@ -12,7 +12,16 @@ public class stringManipulation {
 	String s1="Allan Alda";
 	String s2 ="John Wayne";
 	String s3 = "Gregory Peck";
-	System.out.println();
+	String fname1 = s1.substring(2,5);
+	String lname1 = s1.substring(6,7);
+	String fname2 = s2.substring(2,4);
+	String lname2 = s2.substring(5,7);
+	String fname3 = s3.substring(2,7);
+	String lname3 = s3.substring(8,9);
+	System.out.println("Allan Alda>>>"+ fname1 + " " + lname1);
+	System.out.println("John Wayne>>>"+ fname2 + " " + lname2);
+	System.out.println("Gregory Pec>>>"+ fname3 + " " + lname3);
+	
 	}
 
 }
