aa71f5431a923ab11e21c6ce7aabbf5a0392e6be
rsullivan <rsullivan@HS-D210-15.MOHS.local>
2019-03-07 13:22:47 -0500

    yer

--- Lesson 3- purple sheet/src/purplesheet.java
+++ Lesson 3- purple sheet/src/purplesheet.java
@@ -6,6 +6,7 @@ public class purplesheet {
 System.out.println("\\t is a tab, \\n is a new line , \\\" prints a \"quote\"");
 System.out.println("\"line one\" \n \\line\t\ttwo\\");
 System.out.println("\\\\ denotes a single line comment\n \\ *denote");
+System.out.println("\"\\\"\\\"\"\n\t\\\"\\\"\"\\\n\\\"\"\"\"\"\\");
 
 	}
 
--- Lesson 3- string manipulation/src/stringManipulation.java
+++ Lesson 3- string manipulation/src/stringManipulation.java
@@ -12,15 +12,12 @@ public class stringManipulation {
 	String s1="Allan Alda";
 	String s2 ="John Wayne";
 	String s3 = "Gregory Peck";
-	String fname1 = s1.substring(2,5);
-	String lname1 = s1.substring(6,7);
-	String fname2 = s2.substring(2,4);
-	String lname2 = s2.substring(5,7);
-	String fname3 = s3.substring(2,7);
-	String lname3 = s3.substring(8,9);
-	System.out.println(s1 +">>>"+ fname1 + " " + lname1);
-	System.out.println(s2 +">>>"+ fname2 + " " + lname2);
-	System.out.println(s3 +">>>"+ fname3 + " " + lname3);
+	String fname1 = s1.substring(2,s1.length()-3);
+	String fname2 = s2.substring(2,s2.length()-3);
+	String fname3 = s3.substring(2,s3.length()-3);
+	System.out.println(s1 +">>>"+ fname1);
+	System.out.println(s2 +">>>"+ fname2);
+	System.out.println(s3 +">>>"+ fname3);
 	}
 
 }
